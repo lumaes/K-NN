f325087fb0d7c6489cbbfeb5ceb30deebe4d422b
lumaes <lucas.maes@student.unamur.be>
2020-04-05 17:45:31 +0200

    add custom distance choice and make it easier with function

--- knn.py
+++ knn.py
@@ -1,41 +1,5 @@
 
-
-import math
-
-
-def distance(x1,x2):
-	"""
-	Compute the distance between x1 and x2 points.
-
-	note: x1 and x2 need the have the same length
-	
-	Parameters:
-	-----------
-	x1: the first point coordinates (list)
-	x2: the second point coordinates (list)
-
-	Returns:
-	--------
-	dist: the distance between x1 and x2
-	"""
-
-	# --- EUCLIDIAN DISTANCE ---
-
-	#init distance to 0
-	dist = 0
-
-	# for each index of points
-	for i in range(len(x1)):
-		# add the squared difference for the i index of x1 and x2
-		dist += (x1[i] - x2[i]) ** 2  
-
-	# square root of the distance 
-	dist = math.sqrt(dist)
-
-	return dist
-
-
-def knn(dataset, k, querry_point, classification, neighbors = [], weighted = False):
+def knn(dataset, querry_point, k, classification, distance , neighbors = [], weighted = False):
 	"""
 	Implementations of the KNN algorithm
 	for based instanced machine learning.
@@ -55,80 +19,116 @@ def knn(dataset, k, querry_point, classification, neighbors = [], weighted = Fal
 	the result of knn for the querry_point
 	"""
 
-
 	# if algorithm is finish, then return the result wanted
 	if k == 0:
 
 		if not classification:
-
-			# get the sum of all Y coordinate of the neighbors points
-			sum = 0
-			for pt in neighbors:
-				sum += pt[-1]
-			# result is the sum divide by the numbers of neightbours (= k)
-			result = sum / k
-			return result
-
+			#return the mean of all y points
+			return mean_y(neighbors)
 		else:
-
-			result = []
-
-			for pt in neighbors:
-				result.append(pt[-1])
-
+			result = [pt[-1] for pt in neighbors]
 			return max(set(result), key = result.count)
-
-
 	else:
 
 		min_point = dataset[0]
 
 		for point in dataset:
-
 			if distance(point[0], querry_point) <= distance(min_point[0], querry_point):
 
 				min_point = point
 
-
 		neighbors.append(min_point)
 		dataset.remove(min_point)
 
-		return knn(dataset, k-1, querry_point, classification, neighbors)
+		return knn(dataset, querry_point, k=(k-1), classification=classification, distance=distance , neighbors= neighbors)
 
 
+def euclidian_distance(x1,x2):
+	"""
+	Compute the distance between x1 and x2 points.
+
+	note: x1 and x2 need the have the same length
+	
+	Parameters:
+	-----------
+	x1: the first point coordinates (list)
+	x2: the second point coordinates (list)
+
+	Returns:
+	--------
+	dist: the distance between x1 and x2
+	"""
+
+	#init distance to 0
+	dist = 0
+
+	# for each index of points
+	for i in range(len(x2)):
+		# add the squared difference for the i index of x1 and x2
+		dist += (x1[i] - x2[i]) ** 2  
+
+	# square root of the distance 
+	dist = dist ** (1/2)
+
+	return dist
 
 
-data = [[[42], 1],
-		[[65], 1],
-		[[50], 1],
-		[[76], 1],
-		[[96], 1],
-		[[50], 1],
-		[[91], 0],
-		[[58], 1],
-		[[25], 1],
-		[[23], 1],
-		[[75], 1],
-		[[46], 0],
-		[[87], 0],
-		[[96], 0],
-		[[45], 0],
-		[[32], 1],
-		[[63], 0],
-		[[21], 1],
-		[[26], 1],
-		[[93], 0],
-		[[68], 1],
-		[[96], 0], ]
+def manhattan_distance(x1, x2):
+	"""
+	"""
 
-print(knn(data, 3, [33], True))
+	# init the distance between x1 and x2 to 0.
+	dist = 0
+
+	# for each dimensions of the points
+	for i in range(len(x2)):
+		# add absolute difference of  i eme componant of x1 and x2
+		dist += abs(x1[i] - x2[i])
+
+	return dist
+
+def abs(number):
+	if number < 0:
+		number *= -1
+	return number
+
+def mean_y(array):
+	sum = 0
+	for pt in array:
+		#get the y value for each point
+		sum += pt[-1]
+
+	return sum / len(array)
+
+def main():
+	#data set
+	data = [[[42], 1],
+			[[65], 1],
+			[[50], 1],
+			[[76], 1],
+			[[96], 1],
+			[[50], 1],
+			[[91], 0],
+			[[58], 1],
+			[[25], 1],
+			[[23], 1],
+			[[75], 1],
+			[[46], 0],
+			[[87], 0],
+			[[96], 0],
+			[[45], 0],
+			[[32], 1],
+			[[63], 0],
+			[[21], 1],
+			[[26], 1],
+			[[93], 0],
+			[[68], 1],
+			[[96], 0], ]
+
+	result = knn(data, [33], k=3, classification=True, distance=manhattan_distance)
+
+	print(result)
 
 
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
