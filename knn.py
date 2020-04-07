d6ffae73b32a064605ef6c52dde483457bdc30cd
lumaes <lucas.maes@student.unamur.be>
2020-04-07 20:33:00 +0200

    add more spec'

--- knn.py
+++ knn.py
@@ -26,18 +26,21 @@ def knn(dataset, querry_point, k, classification, distance , neighbors = [], wei
 			#return the mean of all y points
 			return mean_y(neighbors)
 		else:
+			# get all label of the neighbors
 			result = [pt[-1] for pt in neighbors]
 			return max(set(result), key = result.count)
 	else:
 
-		min_point = dataset[0]
 
+		min_point = dataset[0]
+		# get the minimum distance of the dataset
 		for point in dataset:
 			if distance(point[0], querry_point) <= distance(min_point[0], querry_point):
-
 				min_point = point
 
+		# add min point to neighbors list
 		neighbors.append(min_point)
+		# remove the min_point from dataset
 		dataset.remove(min_point)
 
 		return knn(dataset, querry_point, k=(k-1), classification=classification, distance=distance , neighbors= neighbors)
@@ -45,7 +48,8 @@ def knn(dataset, querry_point, k, classification, distance , neighbors = [], wei
 
 def euclidian_distance(x1,x2):
 	"""
-	Compute the distance between x1 and x2 points.
+	Compute the distance between x1 and x2 points 
+	using Euclidian's distance.
 
 	note: x1 and x2 need the have the same length
 	
@@ -75,6 +79,19 @@ def euclidian_distance(x1,x2):
 
 def manhattan_distance(x1, x2):
 	"""
+	Compute the distance between x1 and x2 points 
+	using Manhattan's distance.
+
+	note: x1 and x2 need the have the same length
+	
+	Parameters:
+	-----------
+	x1: the first point coordinates (list)
+	x2: the second point coordinates (list)
+
+	Returns:
+	--------
+	dist: the distance between x1 and x2
 	"""
 
 	# init the distance between x1 and x2 to 0.
@@ -88,47 +105,39 @@ def manhattan_distance(x1, x2):
 	return dist
 
 def abs(number):
+	"""
+	Compute absolute value for a given number.
+
+	Parameters:
+	-----------
+	number: a number (float)
+
+	Return:
+	-------
+	number: the absolute value of number (float)
+	"""
+
 	if number < 0:
 		number *= -1
 	return number
 
 def mean_y(array):
+	"""
+	Compute the mean for a given array
+
+	Parameters:
+	-----------
+	array: a array which contains numbers (float)
+
+	Return:
+	-------
+	sum/len(array): the mean of array (float)
+	"""
+
+
 	sum = 0
 	for pt in array:
 		#get the y value for each point
 		sum += pt[-1]
 
 	return sum / len(array)
-
-def main():
-	#data set
-	data = [[[42], 1],
-			[[65], 1],
-			[[50], 1],
-			[[76], 1],
-			[[96], 1],
-			[[50], 1],
-			[[91], 0],
-			[[58], 1],
-			[[25], 1],
-			[[23], 1],
-			[[75], 1],
-			[[46], 0],
-			[[87], 0],
-			[[96], 0],
-			[[45], 0],
-			[[32], 1],
-			[[63], 0],
-			[[21], 1],
-			[[26], 1],
-			[[93], 0],
-			[[68], 1],
-			[[96], 0], ]
-
-	result = knn(data, [33], k=3, classification=True, distance=manhattan_distance)
-
-	print(result)
-
-
-if __name__ == '__main__':
-    main()
